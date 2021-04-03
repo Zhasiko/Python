a5b1aec06da90d33d5ca35d26fbdf1c8e198dc47
Zhassulan <zhassulanissayev@gmail.com>
2021-04-03 22:47:25 +0600

    upd

--- TSIS7/func.py
+++ TSIS7/func.py
@@ -5,8 +5,6 @@ pygame.init()
 screen = pygame.display.set_mode(SIZE)
 
 play = True
-PI = math.pi
-cnt = 0
 
 while play:
     for event in pygame.event.get():
@@ -19,9 +17,9 @@ while play:
     pygame.draw.line(screen, BLACK, (70, 280), (730, 280), 3)  # X-Axis
     pygame.draw.line(screen, BLACK, (400, 10), (400, 550), 3)  # Y-Axis
 
-    pygame.draw.line(screen, BLACK, (70, 40), (730, 40))  # H_line 1
+    #pygame.draw.line(screen, BLACK, (70, 40), (730, 40))  # H_line 1
 
-    pygame.draw.line(screen, BLACK, (70, 520), (730, 520))  # H_line 2
+    #pygame.draw.line(screen, BLACK, (70, 520), (730, 520))  # H_line 2
     pygame.draw.line(screen, BLACK, (100, 10), (100, 550))  # V_line 1
     pygame.draw.line(screen, BLACK, (700, 10), (700, 550))  # V_line 2
     
@@ -29,6 +27,7 @@ while play:
     for x in range(100, 701, 100):
         pygame.draw.line(screen, BLACK, (x, 10), (x, 550))
         
+
     for x in range(100, 701, 50):
 
         pygame.draw.line(screen, BLACK, (x, 10), (x, 30))
@@ -53,13 +52,13 @@ while play:
 
     for x in range(100, 700):
 
-        cos_y1 = 240 * math.cos((x - 100) / 100 * PI)
-        cos_y2 = 240 * math.cos((x - 99) / 100 * PI)
+        cos_y1 = 240 * math.cos((x - 100) / 100 * math.pi)
+        cos_y2 = 240 * math.cos((x - 99) / 100 * math.pi)
         pygame.draw.aalines(screen, BLUE, False, [(x, 280 + cos_y1), ((x + 1), 280 + cos_y2)])
 
 
-        sin_y1 = 240 * math.sin((x - 100) / 100 * PI)
-        sin_y2 = 240 * math.sin((x - 99) / 100 * PI)
+        sin_y1 = 240 * math.sin((x - 100) / 100 * math.pi)
+        sin_y2 = 240 * math.sin((x - 99) / 100 * math.pi)
         pygame.draw.aalines(screen, RED, False, [(x, 280 + sin_y1), ((x + 1), 280 + sin_y2)])
 
 
